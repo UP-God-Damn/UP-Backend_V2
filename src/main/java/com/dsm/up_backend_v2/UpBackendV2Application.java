5e091a24e32f58d0c74d8fab42d1e3cd6e87bbc6
soohyeon <qpsxhf410@outlook.kr>
2023-09-05 19:16:17 +0900

    ⚙️ SETTING :: 기본 세팅

---
+++ src/main/java/com/dsm/up_backend_v2/UpBackendV2Application.java
@@ -0,0 +1,13 @@
+package com.dsm.up_backend_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpBackendV2Application.class, args);
+	}
+
+}
