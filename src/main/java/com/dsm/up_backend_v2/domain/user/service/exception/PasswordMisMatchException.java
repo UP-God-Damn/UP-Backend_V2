68bf49ea5ac569b06fc6f3d969cbe297841e3490
soohyeon <qpsxhf410@outlook.kr>
2023-09-26 23:33:22 +0900

    🐚 UPDATE :: user 에러코드 수정

---
+++ src/main/java/com/dsm/up_backend_v2/domain/user/service/exception/PasswordMisMatchException.java
@@ -0,0 +1,12 @@
+package com.dsm.up_backend_v2.domain.user.service.exception;
+
+import com.dsm.up_backend_v2.global.error.exception.BaseException;
+import com.dsm.up_backend_v2.global.error.exception.ErrorCode;
+
+public class PasswordMisMatchException extends BaseException {
+    private final BaseException EXCEPTION = new PasswordMisMatchException();
+
+    public PasswordMisMatchException() {
+        super(ErrorCode.PASSWORD_MISMATCH);
+    }
+}
--- src/main/java/com/dsm/up_backend_v2/global/error/ErrorResponse.java
+++ src/main/java/com/dsm/up_backend_v2/global/error/ErrorResponse.java
@@ -1,6 +1,5 @@
 package com.dsm.up_backend_v2.global.error;
 
-import com.dsm.up_backend_v2.global.error.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 
