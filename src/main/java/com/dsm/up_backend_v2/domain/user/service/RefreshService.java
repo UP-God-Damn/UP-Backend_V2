e2dbdef335011ae8ad612bef5694ea4adf57c514
soohyeon <qpsxhf410@outlook.kr>
2023-10-02 23:17:31 +0900

    🐚 UPDATE :: RefreshService 중복코드 및 에러코드 수정

--- src/main/java/com/dsm/up_backend_v2/domain/user/service/RefreshService.java
+++ src/main/java/com/dsm/up_backend_v2/domain/user/service/RefreshService.java
@@ -1,6 +1,7 @@
 package com.dsm.up_backend_v2.domain.user.service;
 
 import com.dsm.up_backend_v2.domain.user.presentation.dto.response.TokenResponse;
+import com.dsm.up_backend_v2.domain.user.service.exception.JwtInvalidException;
 import com.dsm.up_backend_v2.domain.user.service.exception.UserNotFoundException;
 import com.dsm.up_backend_v2.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +15,11 @@ public class RefreshService {
     private final JwtProvider jwtProvider;
 
     public TokenResponse refresh(String refreshToken) {
-        if(!jwtProvider.validate(refreshToken)) throw UserNotFoundException.EXCEPTION;
-        String nAccessToken = jwtProvider.generateToken(jwtProvider.getSubject(refreshToken));
-        String nRefreshToken = jwtProvider.generateRefreshToken(jwtProvider.getSubject(refreshToken));
+        if(!jwtProvider.validate(refreshToken)) throw JwtInvalidException.EXCEPTION;
+
+        String subject = jwtProvider.getSubject(refreshToken);
+        String nAccessToken = jwtProvider.generateToken(subject);
+        String nRefreshToken = jwtProvider.generateRefreshToken(subject);
 
         return TokenResponse.builder()
                 .accessToken(nAccessToken)
