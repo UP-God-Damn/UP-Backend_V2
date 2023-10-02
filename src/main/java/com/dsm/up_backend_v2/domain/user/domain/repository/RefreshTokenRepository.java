f7cc4d5aa6826737594dc215b7bde1df33083c7e
soohyeon <qpsxhf410@outlook.kr>
2023-10-03 00:03:23 +0900

    🐚 UPDATE :: 토큰 재발급 코드 수정

--- src/main/java/com/dsm/up_backend_v2/domain/user/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/dsm/up_backend_v2/domain/user/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.dsm.up_backend_v2.domain.user.domain.repository;
 import com.dsm.up_backend_v2.domain.user.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
--- src/main/java/com/dsm/up_backend_v2/domain/user/service/RefreshService.java
+++ src/main/java/com/dsm/up_backend_v2/domain/user/service/RefreshService.java
@@ -1,8 +1,9 @@
 package com.dsm.up_backend_v2.domain.user.service;
 
+import com.dsm.up_backend_v2.domain.user.domain.repository.RefreshTokenRepository;
 import com.dsm.up_backend_v2.domain.user.presentation.dto.response.TokenResponse;
 import com.dsm.up_backend_v2.domain.user.service.exception.JwtInvalidException;
-import com.dsm.up_backend_v2.domain.user.service.exception.UserNotFoundException;
+import com.dsm.up_backend_v2.domain.user.service.exception.RefreshTokenNotFoundException;
 import com.dsm.up_backend_v2.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class RefreshService {
     private final JwtProvider jwtProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public TokenResponse refresh(String refreshToken) {
         if(!jwtProvider.validate(refreshToken)) throw JwtInvalidException.EXCEPTION;
+        refreshTokenRepository.findByRefreshToken(refreshToken).orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
 
         String subject = jwtProvider.getSubject(refreshToken);
         String nAccessToken = jwtProvider.generateToken(subject);
