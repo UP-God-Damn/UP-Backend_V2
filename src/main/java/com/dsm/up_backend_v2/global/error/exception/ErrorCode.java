d55d359597709912e647991c179867254b37e64b
lee na kyung <ni1722650@gmail.com>
2023-10-30 09:20:07 +0900

    🐚 UPDATE :: ErrorCode 수정

--- src/main/java/com/dsm/up_backend_v2/global/error/exception/ErrorCode.java
+++ src/main/java/com/dsm/up_backend_v2/global/error/exception/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     //Bad Request
     BAD_REQUEST(400, "잘못된 요청입니다."),
     PASSWORD_MISMATCH(400, "비밀번호가 일치하지 않습니다."),
+    IMAGE_UPLOAD_FAIL(400, "이미지가 정상적으로 업로드 되지 않았습니다."),
+    IMAGE_FORM_FALSE(400, "이미지 형식이 올바르지 않습니다."),
 
     //UnAuthorised
     NOT_ACCESS_TOKEN(401, "access 토큰이 아닙니다."),
