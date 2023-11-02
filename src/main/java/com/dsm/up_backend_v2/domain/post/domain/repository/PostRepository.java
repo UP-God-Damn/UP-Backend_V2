0088113f577acce9f790649393e542e0d6387a8b
velyn666 <ni1722650@gmail.com>
2023-11-02 23:02:51 +0900

    🐚 UPDATE :: PostRepository 수정

--- src/main/java/com/dsm/up_backend_v2/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/dsm/up_backend_v2/domain/post/domain/repository/PostRepository.java
@@ -1,7 +1,18 @@
 package com.dsm.up_backend_v2.domain.post.domain.repository;
 
 import com.dsm.up_backend_v2.domain.post.domain.Post;
+import com.dsm.up_backend_v2.domain.post.domain.type.Major;
+import com.dsm.up_backend_v2.domain.post.domain.type.State;
+import com.dsm.up_backend_v2.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+    Page<Post> findAllByTitleContainingOrderByIdDesc(String title, Pageable pageable);
+    Page<Post> findAllByTitleContainingAndMajorOrderByIdDesc(String title, Major major, Pageable pageable);
+    Page<Post> findAllByStateAndTitleContainingAndMajorOrderByIdDesc(State state, String title, Major major, Pageable pageable);
+    Page<Post> findAllByStateAndTitleContainingOrderByIdDesc(State state, String title, Pageable pageable);
+    Page<Post> findAllByUserOrderByIdDesc(User user, Pageable pageable);
+    long countByUser(User user);
 }
